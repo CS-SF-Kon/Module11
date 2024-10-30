97f57375ca234b7157a088c820cc04c4f9a5bb05
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-10-30 10:52:19 +0700

    Добавьте файлы проекта.

---
+++ Module11.MainTGBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module11.MainTGBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
